d549f4a1a9ab7333007e66c5f68045c3f4578546
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-09-12 15:51:21 +0900

    feat: create member resolver & service model

--- apps/rentcar-api/src/components/member/member.resolver.ts
+++ apps/rentcar-api/src/components/member/member.resolver.ts
@@ -1,4 +1,34 @@
-import { Resolver } from '@nestjs/graphql';
+import { Mutation, Resolver, Query } from '@nestjs/graphql';
+import { MemberService } from './member.service';
 
 @Resolver()
-export class MemberResolver {}
+export class MemberResolver {
+	constructor(private readonly memberService: MemberService) {}
+	/** ================ signup ============== */
+	@Mutation(() => String)
+	public async signup(): Promise<string> {
+		console.log('Mutation: signup');
+		return this.memberService.signup();
+	}
+
+	/** ================ login ============== */
+	@Mutation(() => String)
+	public async login(): Promise<string> {
+		console.log('Mutation: login');
+		return this.memberService.login();
+	}
+
+	/** ================ updateMember ============== */
+	@Mutation(() => String)
+	public async updateMember(): Promise<string> {
+		console.log('Mutation: updateMember');
+		return this.memberService.updateMember();
+	}
+
+	/** ================ getMember ============== */
+	@Query(() => String)
+	public async getMember(): Promise<string> {
+		console.log('Query: getMember');
+		return this.memberService.getMember();
+	}
+}
